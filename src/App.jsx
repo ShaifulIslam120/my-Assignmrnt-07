ce7e8f0800bf5f73d5c124c7153e60eadcc51ff9
shaiful <shaifulcit@gmil.com>
2024-10-24 19:46:53 +0600

    2ndt

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './components/component/header/Header'
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+<body>
+  <Header></Header>
+</body>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/component/header/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <img src="/src/components/component/image/logo.png" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
